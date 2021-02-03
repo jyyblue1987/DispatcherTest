266427c763e7b8927692457246ca9b8981df7471
jyyblue <jyyblue1987@outlook.com>
2021-02-03 11:46:01 +0800

    [add] add some comments.

--- DispatcherTest/builder.cpp
+++ DispatcherTest/builder.cpp
@@ -30,8 +30,25 @@ void builder::freeMemory()
 
 void builder::addRequest(int sectorNum, structure_type type)
 {
+	// Each request specifies a sector and type of structure to build.
 	event ev(sectorNum, type);
+
+	// Whenever the builder receives a request, it should print out a message in the following format
+	switch(type)
+	{
+	case WIND:		
+		printf("Builder #%d: Received request to build a wind turbine in sector %d\n", m_nBuilderNum, sectorNum);
+		break;
+	case WATER:		
+		printf("Builder #%d: Received request to build a water harvester in sector %d\n", m_nBuilderNum, sectorNum);
+		break;
+	case SOLAR:		
+		printf("Builder #%d: Received request to build a solar energy collector in sector %d\n", m_nBuilderNum, sectorNum);
+		break;
+	}
 	
+	// The requests are stored in a linked list in the builder
+	// New requests must be placed at the front of the list.	
 	req->insertFront(ev);
 }
 
@@ -40,12 +57,13 @@ bool builder::doCycle()
 	if( req->isEmpty() )
 		return false;
 
+	// the builder must process the oldest building request
 	event ev;
 	req->removeBack(ev);
 
 	int sector = ev.getSector();
 
-	if( m_nPrevSector != ev.getSector() )
+	if( m_nPrevSector != ev.getSector() )	// Otherwise, the builder must travel to the new sector first.
 	{
 		// Moving sector
 		event ev1(sector, UNK);
@@ -54,7 +72,9 @@ bool builder::doCycle()
 		printf("Builder #%d: Moving to sector %d\n", m_nBuilderNum, sector);
 		m_nPrevSector = sector;
 	}
-
+ 
+	// Otherwise, the builder must travel to the new sector first.
+	// and create that structure.
 	st->push(ev);
 	switch(ev.getType())
 	{
@@ -75,6 +95,8 @@ bool builder::doCycle()
 
 void builder::returnHome() 
 {
+	// When this happens, 
+	// the builder should backtrack through the list of structures that it built, in reverse order.
 	while(st->isEmpty() == false)
 	{
 		event *ev = st->pop();
